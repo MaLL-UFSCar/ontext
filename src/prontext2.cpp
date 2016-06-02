b88176f9c3bf81344309e73b10b51219bb2d732f
diorge <diorge.bs@gmail.com>
2016-06-02 15:58:34 -0300

    Category reading

--- src/prontext2.cpp
+++ src/prontext2.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <unordered_set>
 #include <map>
+#include <string>
 
 /**
  * argv[1]: Categories file
@@ -10,5 +11,13 @@
  */
 
 int main (int argc, char** argv) {
+   std::map<std::string, std::unordered_set<std::string> > instances;
 
+   std::ifstream categoriesFile(argv[1]);
+   std::string category1;
+   std::string category2;
+   int categoryid;
+
+   while (categoriesFile >> category1 >> category2 >> categoryid) {
+   }
 }
