1e20fc8d83e7a62c0a8bc1e4b8c36ce67180c632
diorge <diorge.bs@gmail.com>
2016-06-02 16:08:41 -0300

    Instances mappings construction

--- src/prontext2.cpp
+++ src/prontext2.cpp
@@ -11,13 +11,25 @@
  */
 
 int main (int argc, char** argv) {
-   std::map<std::string, std::unordered_set<std::string> > instances;
+   std::map<std::string, std::unordered_set<std::string>* > instances;
 
    std::ifstream categoriesFile(argv[1]);
    std::string category1;
    std::string category2;
    int categoryid;
+   std::string instanceDir(argv[2]);
+   instanceDir += "/";
 
    while (categoriesFile >> category1 >> category2 >> categoryid) {
+      if (instances.count(category1) == 0) {
+         instances[category1] = new std::unordered_set<std::string>;
+         instances[category1]->reserve(8192);
+         std::ifstream instanceFile(instanceDir + category1);
+         std::string seed;
+         while (instanceFile >> seed) {
+            instances[category1]->insert(seed);
+         }
+      }
    }
+
 }
