0963a78dc77eedebcc1c752b1d67046c0bc6ddf6
diorge <diorge.bs@gmail.com>
2016-06-02 15:53:32 -0300

    Algorithm sketch

---
+++ src/prontext2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_set>
+#include <map>
+
+/**
+ * argv[1]: Categories file
+ * argv[2]: Instances directory
+ * argv[3]: SVO file
+ */
+
+int main (int argc, char** argv) {
+
+}
